3709d9925bf39ac7070eadefbd03980c7beff547
mrcodegit <mrcode@gmail.com>
2020-06-18 14:03:03 +0200

    callback in nested component

--- episode2/react-app/src/App.tsx
+++ episode2/react-app/src/App.tsx
@@ -1,29 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, ChangeEvent } from 'react';
 import Person from './component/Person';
 import './App.scss';
 
 class App extends Component<{}, any> {
 
-  cls = "App";
-
-  test = () => {
-    console.log('test');
-  }
-
+  newPersonName: string = '';
   constructor(props: any) {
     super(props);
     this.state = {
+      inputValue: '',
       persons: ['Ragnar', 'Lagertha']
     }
   }
+
+  change = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
+  addPerson(name: string){
+    this.setState((state: any) =>{
+      const persons = state.persons.concat(name);
+      return {
+        persons
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>React vs Angular</h1>
         <div className="input-container">
           <label>name</label>
-          <input id="name" type="text" />
-          <button>ADD</button>
+          <input id="name" type="text" onChange={e => this.change(e)} />
+          <button onClick = {() => {this.addPerson(this.state.inputValue)}} >ADD</button>
         </div>
         <div className="persons-list">
           {this.state.persons.map((person: string, i: number) => {
