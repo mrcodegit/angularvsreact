a88aa696c2d8b42c53c358b64ad922168360b816
mrcodegit <mrcode@gmail.com>
2020-08-24 00:26:22 +0200

    episode 3 angular start

--- episode3/angular-app/src/modules/layout/layout.module.ts
+++ episode3/angular-app/src/modules/layout/layout.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeroesModule } from '../heroes/heroes.module';
-import { CitiesModule } from '../cities/cities.module';
-import { NavigationModule } from '../navigation/navigation.module';
-import { LayoutComponent } from './layout.component';
 
+import { LayoutRoutingModule } from './layout-routing.module';
+import { NavigationModule } from '../navigation/navigation.module';
+import { CitiesModule } from '../cities/cities.module';
+import { HeroesModule } from '../heroes/heroes.module';
+import { LayoutComponent } from './layout.component';
 
 
 @NgModule({
   declarations: [LayoutComponent],
   imports: [
     CommonModule,
-    HeroesModule,
+    LayoutRoutingModule,
+    NavigationModule,
     CitiesModule,
-    NavigationModule
+    HeroesModule
   ],
   exports: [LayoutComponent]
 })
