ffd01eafa8a465d7c0a9d4b891712c28774ef1c0
mrcodegit <mrcode@gmail.com>
2020-08-25 00:22:15 +0200

    clean up

--- episode3/angular-app/src/modules/layout/layout.module.ts
+++ episode3/angular-app/src/modules/layout/layout.module.ts
@@ -1,21 +1,37 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { LayoutRoutingModule } from './layout-routing.module';
-import { NavigationModule } from '../navigation/navigation.module';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CitiesComponent } from '../cities/cities.component';
 import { CitiesModule } from '../cities/cities.module';
+import { HeroesComponent } from '../heroes/heroes.component';
 import { HeroesModule } from '../heroes/heroes.module';
+import { NavigationModule } from '../navigation/navigation.module';
 import { LayoutComponent } from './layout.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: 'heroes'
+  },
+  {
+    path: 'cities',
+    component: CitiesComponent
+  }, {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+];
 
 
 @NgModule({
   declarations: [LayoutComponent],
   imports: [
     CommonModule,
-    LayoutRoutingModule,
     NavigationModule,
     CitiesModule,
-    HeroesModule
+    HeroesModule,
+    RouterModule.forRoot(routes)
   ],
   exports: [LayoutComponent]
 })
